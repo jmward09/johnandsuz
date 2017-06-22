13f5b8a6f9f1f17917e94724af077b97c89604d6
John Ward <jward@guthy-renker.com>
2017-06-21 20:58:57 -0700

    wires up foundation

--- src/app/invite.component.ts
+++ src/app/invite.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'wedding-invite',
   templateUrl: './invite.component.html',
-  styleUrls: ['./invite.component.css']
+  styleUrls: ['./invite.component.scss']
 })
 export class InviteComponent{
   title = "Wedding Site";
